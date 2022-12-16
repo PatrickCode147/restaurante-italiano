325211c602de590f1f2460fad74f7937b5732a87
patrifck@hotmail.com <106826397+PatrickCode147@users.noreply.github.com>
2022-12-15 22:07:42 -0300

    att delete

--- script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav_menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
